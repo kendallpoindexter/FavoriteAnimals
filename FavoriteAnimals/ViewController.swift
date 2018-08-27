3a4d5673def146b6f5e6f3049b5224cefb01ce71
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-08-27 16:22:59 -0400

    Initial commit

---
+++ FavoriteAnimals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavoriteAnimals
+//
+//  Created by Kendall Poindexter on 8/27/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
