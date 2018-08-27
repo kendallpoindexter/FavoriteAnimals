757479e6d30437ed62977ecfc8d87893aa6ffbc0
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-08-27 16:54:37 -0400

    Added required protocols and an array of my favorite animals

--- FavoriteAnimals/FavoriteAnimalViewController.swift
+++ FavoriteAnimals/FavoriteAnimalViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class FavoriteAnimalViewController: UITableViewController {
 
+// MARK: - Properties
+    
+    let myFavoriteAnimals = ["Lions", "Tigers", "Dogs", "Tortoises", "Elephants"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,18 @@ class FavoriteAnimalViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+// MARK: - Methods
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteAnimals", for: indexPath)
+        cell.textLabel?.text = myFavoriteAnimals[indexPath.row]
+        
+        return cell
+    }
 }
 
